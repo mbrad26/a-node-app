c9f52054550fc46fc842374bba06f268bea38ca4
mbrad26 <mbrad26@gmail.com>
2020-09-06 17:34:08 +0100

    install cross-fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import 'dotenv/config';
+import 'cross-fetch/polyfill';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
 const userCredentials = { firstname: 'Robin' };
 const userDetails = { nationality: 'German' };
